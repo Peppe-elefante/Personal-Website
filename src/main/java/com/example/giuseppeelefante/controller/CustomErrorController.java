0c7b5fd1944d47c9c074bc0fb17036073b3fb203
Peppe-elefante <giuseppeelefante123@gmail.com>
2024-10-20 18:15:11 +0200

    fixed home bug

---
+++ src/main/java/com/example/giuseppeelefante/controller/CustomErrorController.java
@@ -0,0 +1,22 @@
+package com.example.giuseppeelefante.controller;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CustomErrorController implements
+        ErrorController {
+
+    @GetMapping("/error")
+    public String handleError() {
+        return "homepage";
+    }
+
+
+}
